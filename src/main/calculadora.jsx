fceed218ee100c9bd7974caa0965b29068d9d4a6
Claudinei <claudineisilva.ti@fundecc.org.br>
2024-05-01 16:48:15 -0300

    Criação do corpo da calculadora

---
+++ src/main/calculadora.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from "react";
+import './calculadora.css';
+
+
+export default class Calcuadora extends Component {
+
+    render() {
+        return (
+            <div className="calculadora">
+
+            </div>
+        )
+    }
+}
